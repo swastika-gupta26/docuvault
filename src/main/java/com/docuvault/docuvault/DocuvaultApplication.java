76aece7161c669ec81fdaee44582417083f221a1
Swastika <riddhigupta656@gmail.com>
2026-09-05 23:22:15 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/docuvault/docuvault/DocuvaultApplication.java
@@ -0,0 +1,13 @@
+package com.docuvault.docuvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocuvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docuvault/docuvault/DocuvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docuvault.docuvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
